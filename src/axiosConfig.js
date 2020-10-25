fb2d4e34c800f1f96ec573f6a6f1c478ef7a3d37
Tim Shaker <tim@tmshkr.com>
2020-10-24 22:54:20 -0700

    update API

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 const token = localStorage.getItem("token");
-axios.defaults.baseURL = "https://devdeskqueue3-pt.herokuapp.com/";
+axios.defaults.baseURL = "https://dev-desk-node.herokuapp.com/";
 axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
