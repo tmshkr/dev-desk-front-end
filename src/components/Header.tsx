49c190164a555ef942744f301c0e580aa547305b
Tim Shaker <tim@tmshkr.com>
2020-10-24 17:25:44 -0700

    styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Box, Heading, Text, Tag } from "@chakra-ui/core";
 
 export interface Props {}
 
 function Header(props: Props) {
   return (
-    <Box
-      as="header"
-      p={3}
-      display="flex"
-      justifyContent="flex-end"
-      backgroundColor="blue.900"
-      color="white"
-    >
-      Header.tsx
+    <Box as="header" p={3} backgroundColor="blue.900" color="white">
+      <Box
+        maxWidth={800}
+        margin="auto"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Heading as="h2">DevDesk</Heading>
+        <Box as="nav">
+          <Link to="/dashboard">Dashboard</Link>
+        </Box>
+      </Box>
     </Box>
   );
 }
